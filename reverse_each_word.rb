3ad33173292f24255e12876a40e23c9febac09e8
Kim Gonzales <kimgo589@gmail.com>
2018-01-25 17:38:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,14 @@
 
+#def reverse_each_word(string)
+#  array = string.split
+#  array.collect do |word|
+#    word.reverse
+#  end
+#  .join(" ")
+#end
+
 def reverse_each_word(string)
-  array = string.split
-  array.collect do |word|
+  string.split.collect do | word |
     word.reverse
   end
-  .join(" ")
 end
